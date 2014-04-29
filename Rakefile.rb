8678c3c1ab1c94cced419126cb548005afe02882
Federico Borgnia <fborgnia@gmail.com>
2014-04-29 11:38:27 -0500

    new gem release

--- Rakefile.rb
+++ Rakefile.rb
@@ -12,12 +12,12 @@ gemspec = Gem::Specification.new do |s|
   s.rubyforge_project = 'riemann-mongodb'
 
   s.name = 'riemann-mongodb'
-  s.version = '0.1.1'
+  s.version = '0.3.0'
   s.author = 'Fede Borgnia'
   s.email = 'fborgnia@gmail.com'
   s.homepage = 'https://github.com/riemann/riemann-mongodb'
   s.platform = Gem::Platform::RUBY
-  s.summary = 'MongoDB client that submits statistics to Riemann.'
+  s.summary = 'MongoDB clients to submits metrics to Riemann.'
 
   s.add_dependency 'riemann-tools', '>= 0.2.1'
   s.add_dependency 'mongo', '>= 1.10.0'
